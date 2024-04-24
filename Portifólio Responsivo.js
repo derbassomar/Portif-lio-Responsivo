2a1844c6f1371527cff741e0b756bbe669b007d2
derbassomar <148890555+derbassomar@users.noreply.github.com>
2024-04-24 15:56:12 -0300

    Add files via upload

---
+++ Portifólio Responsivo.js
@@ -0,0 +1,16 @@
+
+// Portifólio Responsivo.js
+
+document.addEventListener('DOMContentLoaded', () => {
+    const toggle = document.querySelector("#toggle");
+    let isLight = true;
+  
+    function modeSwitch() {
+      isLight = !isLight;
+      document.body.style.backgroundColor = isLight ? "var(--font-color)" : "var(--bg-color)";
+      document.body.style.color = isLight ? "var(--bg-color)" : "var(--font-color)";
+      toggle.innerText = isLight ? "🌞" : "🌒";
+    }
+  
+    toggle.addEventListener("click", modeSwitch);
+  });
